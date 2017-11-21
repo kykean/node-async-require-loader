c00ee8ab86f946b0ffc9884e36720857c3bfbe7f
Khor Yit Kean (YK) <yk.khor@photobookworldwide.com>
2017-11-21 17:59:27 +0800

    upgraded to suits webpack 3

--- index.js
+++ index.js
@@ -1,82 +1,91 @@
- "use strict";
- var loaderUtils = require('loader-utils');
- var __ = require("lodash");
- var path = require("path");
- var _eval = require("eval");
- var requestSync = require("sync-request");
- var fs = require("fs");
+"use strict";
+var loaderUtils = require('loader-utils');
+var __ = require("lodash");
+var path = require("path");
+var _eval = require("eval");
+var requestSync = require("sync-request");
+var fs = require("fs");
 
 
- module.exports = function(content) {
+module.exports = function(content) {
 
-     var that = this;
-     var query = loaderUtils.parseQuery(this.query);
-     var async = (typeof(query.async) !== 'undefined') ? query.async: true;
+    var that = this;
+    var options = loaderUtils.getOptions(this);
+    var query = {};
 
-     try {
-         content = _eval(content);
-     } catch (e) {
-         throw new Error('The .ajs file is not a valid js. ' + e.toString());
-     }
+    if (this.resourceQuery) {
+      query = loaderUtils.parseQuery(this.resourceQuery);
+    }
+    var async = (typeof(query.async) !== 'undefined') ? query.async: true;
 
-     if (Object.prototype.toString.call(content) !== '[object Object]') {
-         throw new Error('The .ajs file does not return a valid json');
-     } else if (typeof content.remoteUrl === "undefined") {
-         throw new Error('The .ajs file must return a remoteUrl');
-     }
-     //////////////////////
-     //Set up the pre parser
-     ///////////////////////
+    try {
+        content = _eval(content);
+    } catch (e) {
+        throw new Error('The .ajs file is not a valid js. ' + e.toString());
+    }
 
-     //Check the custom preparser first
-     var preParser = __.get(this.options, 'NodeAsycRequirePreParser[' + query.preParser + ']');
+    if (Object.prototype.toString.call(content) !== '[object Object]') {
+        throw new Error('The .ajs file does not return a valid json');
+    } else if (typeof content.remoteUrl === "undefined") {
+        throw new Error('The .ajs file must return a remoteUrl');
+    }
+    //////////////////////
+    //Set up the pre parser
+    ///////////////////////
 
-     //If there if no custom preParser then check the offical preParsers
-     if (typeof preParser === "undefined") {
-         switch (query.preParser) {
-             case "rt":
-                 preParser = require("./preParser/reactTemplate.js");
-                 break;
-             case "multipleRts":
-                 preParser = require("./preParser/multipleReactTemplates.js");
-                 break;
-             default:
-                 preParser = function(rawContent) {
-                     return rawContent;
-                 };
-         }
-     }
-     //////////////////////////////////////////////
-     //Set up the node module url with queryString
-     /////////////////////////////////////////////
-     var queryString = query.queryString || "";
-     var nodeModuleUrl = content.remoteUrl.trim() + queryString.replace(/'/gi, ""); //trim the contents and remove the ''
+    //Check the custom preparser first
+    var preParser = __.get(options, 'NodeAsycRequirePreParser[' + query.preParser + ']');
 
-     if (async) {
-         //Do the aync fetch and return
-         console.log("Remote Node Module Url:" + nodeModuleUrl);
-         var res = requestSync('GET', nodeModuleUrl);
-         var rawContent = res.getBody('utf8');
-         var source;
-         if(typeof(query.useUnescape)!=="undefined" && query.useUnescape){
-            source = preParser(rawContent, true);
-         }else{
-            source = preParser(rawContent);
-         }
-         that.callback(null, source);
+    //If there if no custom preParser then check the offical preParsers
+    if (typeof preParser === "undefined") {
+        switch (query.preParser) {
+            case "rt":
+                preParser = require("./preParser/reactTemplate.js");
+                break;
+            case "multipleRts":
+                preParser = require("./preParser/multipleReactTemplates.js");
+                break;
+            default:
+                preParser = function(rawContent) {
+                    return rawContent;
+                };
+        }
+    }
+    //////////////////////////////////////////////
+    //Set up the node module url with queryString
+    /////////////////////////////////////////////
+    var queryString = query.queryString || "";
+    var nodeModuleUrl = content.remoteUrl.trim() + queryString.replace(/'/gi, ""); //trim the contents and remove the ''
 
-     } else {
-         //load local file
-         var rawContent = fs.readFileSync(content.localPath, {
-             encoding: 'utf8'
-         });
+    if (async) {
+        //Do the aync fetch and return
+        console.log("Remote Node Module Url:" + nodeModuleUrl);
+        var res = requestSync('GET', nodeModuleUrl);
+        var rawContent = res.getBody('utf8');
+        var source;
+        if(typeof(query.useUnescape)!=="undefined" && query.useUnescape){
+           source = preParser(rawContent, true);
+        }else{
+           source = preParser(rawContent);
+        }
 
-         var source = preParser(rawContent);
-         that.resourcePath = content.localPath;
-         that.resource = content.localPath;
-         that.callback(null, source);
+        that.callback(null, source);
+        // return source;
 
-     }
+    } else {
+        //load local file
+        var rawContent = fs.readFileSync(content.localPath, {
+            encoding: 'utf8'
+        });
+
+        var source = preParser(rawContent);
+        that.resourcePath = content.localPath;
+        that.resource = content.localPath;
+        _eval(source);
+
+        that.callback(null, source);
+
+    }
 
 
- };
+};
